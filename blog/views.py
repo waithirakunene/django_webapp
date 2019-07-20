a40dd61de07c1517b027b2dc7a3448b3e8b6ce23
waithirakunene <waithira.kunene@gmail.com>
2019-07-20 23:09:50 +0300

    get started with django framework!

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .models import Post
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #display from the database
+    content = { 
+    "posts": Post.objects.all()
+    }
+    return render(request, 'blog/home.html', content)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':'about'})
+
