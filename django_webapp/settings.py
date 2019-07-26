c1f7c8657f93cb22afaf49cfcbbec66e12304f07
waithirakunene <waithira.kunene@gmail.com>
2019-07-26 10:59:54 +0300

    django webapp

--- django_webapp/settings.py
+++ django_webapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -145,4 +145,5 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
 #EMAIL_HOST_USER = 'user emailid'
-#EMAIL_HOST_PASSWORD = 'password'
+#EMAIL_HOST_PASSWORD = 'password'\
+django_heroku.settings(locals())
